2929e40146b69674892f369f09173b4fab935e86
Domin <dominik.donoch@valueadd.pl>
2021-11-29 10:34:45 +0100

    feat: create api connection

--- src/app/recipes/services/http-recipes.service.ts
+++ src/app/recipes/services/http-recipes.service.ts
@@ -1,34 +1,24 @@
-import { Injectable } from "@angular/core";
-import { catchError, Observable, of, tap, throwError } from "rxjs";
+import { Inject, Injectable } from "@angular/core";
+import {  Observable } from "rxjs";
+
 import { Recipe } from "../models/recipe";
-import { PanelType, SnackbarService } from "../../ui/snackbar/snackbar.service";
+import { HttpClient } from "@angular/common/http";
+import { RecipePayload } from "../models/recipe-payload";
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpRecipesService {
 
-  private _recipes: Recipe[] = [
-    { _id: '1', description: '', ingredients: [], name: 'recipe 1', preparationTimeInMinutes: 5 },
-    { _id: '2', description: '', ingredients: [], name: 'recipe 2', preparationTimeInMinutes: 5 },
-  ];
+  constructor(@Inject('API_URL') private _apiUrl: string,
+              private _httpClient: HttpClient) {
+  }
 
-  constructor(private _snackBarService: SnackbarService) {
+  create(recipePayload: RecipePayload): Observable<Recipe> {
+    return this._httpClient.post<Recipe>(`${this._apiUrl}/recipes`, recipePayload);
   }
 
   getAll(): Observable<Recipe[]> {
-    return of(this._recipes).pipe(
-      tap(this.onSuccess.bind(this)),
-      catchError(this.handleError.bind(this))
-    );
-  }
-
-  onSuccess(): void {
-    this._snackBarService.open('Retrieved data!', PanelType.Success);
-  }
-
-  handleError(error: Error): Observable<never> {
-    this._snackBarService.open('There was an error while fetching data', PanelType.Error);
-    return throwError(() => error);
+    return this._httpClient.get<Recipe[]>(`${this._apiUrl}/recipes`);
   }
 }
